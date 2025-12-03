d988539ebe11ce708fd2dd69ef86384419a12bf5
pvangelatos <p.vangelatos23@hotmail.com>
2025-12-03 10:12:58 +0200

    feat: add mul-app

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+
+    public static void main(String[] args) {
+        System.out.println(multiply(2, 5));
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+}
